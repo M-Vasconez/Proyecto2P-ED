47b8611ab31b0c565357f332af1afb148f5d74f4
Matias Vasconez <68170693+M-Vasconez@users.noreply.github.com>
2021-01-19 00:22:44 -0500

    Se implemento segmentarData y encontrarMin

--- Proyecto2P-ED/src/Archivos/DataSet.java
+++ Proyecto2P-ED/src/Archivos/DataSet.java
@@ -6,28 +6,29 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DataSet {
-     private static HashMap<String,ArrayList<Integer>> dataset = new HashMap<>();
-    private static HashMap<String,Double> gini = new HashMap<>();
-    
-    
-    public static void cargarDatos(String nombre){
+
+    private static HashMap<String, ArrayList<Integer>> dataset = new HashMap<>();
+    private static HashMap<String, Double> gini = new HashMap<>();
+
+    public static void cargarDatos(String nombre) {
         try {
-            List<String> lineas = Files.readAllLines(Paths.get("src/Archivos/"+nombre+".csv"));
+            List<String> lineas = Files.readAllLines(Paths.get("src/Archivos/" + nombre + ".csv"));
             String[] parametros = lineas.remove(0).split(";");
-            for(String par: parametros){
+            for (String par : parametros) {
                 dataset.put(par, new ArrayList<Integer>());
                 gini.put(par, 0.0);
             }
-            for(String linea: lineas){
+            for (String linea : lineas) {
                 String[] lineaSep = linea.split(";");
-                for(int i=0 ; i<lineaSep.length ; i++){
+                for (int i = 0; i < lineaSep.length; i++) {
                     dataset.get(parametros[i]).add(Integer.parseInt(lineaSep[i]));
                 }
             }
         } catch (IOException ex) {
-                System.out.println(ex);
+            System.out.println(ex);
         }
     }
 
@@ -38,51 +39,75 @@ public class DataSet {
     public static HashMap<String, Double> getGini() {
         return gini;
     }
-    
-    private static double obtenerGini(String at1, String at2){
+
+    private static double obtenerGini(String at1, String at2) {
         double at1TT = 0, at1TF = 0, at1FF = 0, at1FT = 0;
         ArrayList<Integer> atri1 = dataset.get(at1);
         ArrayList<Integer> atri2 = dataset.get(at2);
-        
-        for(int i = 0; i<atri1.size() ; i++){
-            if(atri1.get(i)==1){
-                if(atri2.get(i)==1){
-                    at1TT+=1;
+
+        for (int i = 0; i < atri1.size(); i++) {
+            if (atri1.get(i) == 1) {
+                if (atri2.get(i) == 1) {
+                    at1TT += 1;
+                } else {
+                    at1TF += 1;
                 }
-                else{
-                    at1TF+=1;
-                }
-            }
-            else{
-                if(atri2.get(i)==1){
-                    at1FT+=1;
-                }
-                else{
-                    at1FF+=1;
+            } else {
+                if (atri2.get(i) == 1) {
+                    at1FT += 1;
+                } else {
+                    at1FF += 1;
                 }
             }
         }
         return calcularGini(at1TT, at1TF, at1FF, at1FT, atri1.size());
     }
-    
-    private static double calcularGini(double at1TT, double at1TF, double at1FF, double at1FT, double at1TOT){
-        double at1T = at1TT+at1TF,    at1F = at1FF+at1FT;
+
+    private static double calcularGini(double at1TT, double at1TF, double at1FF, double at1FT, double at1TOT) {
+        double at1T = at1TT + at1TF, at1F = at1FF + at1FT;
         double gini = 0;
-        if(at1TF == 0 && at1FT == 0){
-            
-            gini = 1 - Math.pow((at1T/at1TOT),2) - Math.pow((at1F/at1TOT),2);
-        }
-        else{
-            double sub1 = 1 - Math.pow((at1TT/at1T),2) - Math.pow((at1TF/at1T),2);
-            double sub2 = 1 - Math.pow((at1FT/at1F),2) - Math.pow((at1FF/at1F),2);
-            gini = (((at1T)/at1TOT)*sub1) + (((at1F)/at1TOT)*sub2);
+        if (at1TF == 0 && at1FT == 0) {
+
+            gini = 1 - Math.pow((at1T / at1TOT), 2) - Math.pow((at1F / at1TOT), 2);
+        } else {
+            double sub1 = 1 - Math.pow((at1TT / at1T), 2) - Math.pow((at1TF / at1T), 2);
+            double sub2 = 1 - Math.pow((at1FT / at1F), 2) - Math.pow((at1FF / at1F), 2);
+            gini = (((at1T) / at1TOT) * sub1) + (((at1F) / at1TOT) * sub2);
         }
         return gini;
     }
-    
-    public static void actualizarGini(String at1){
-        for(String k : gini.keySet()){
-            gini.replace(k,obtenerGini(k,at1));
+
+    public static void actualizarGini(String at1) {
+        for (String k : gini.keySet()) {
+            gini.replace(k, obtenerGini(k, at1));
         }
+    }
+
+    public static void segmentarData(String atributo) {
+        actualizarGini(atributo);
+        String min = buscarMin();
+        System.out.println(min);
+
+    }
+
+    private static String buscarMin() {
+        Double i = 0.0;
+        String Key = "";
+        for (Map.Entry<String, Double> entry : gini.entrySet()) {
+            if (i == 0) {
+                i = entry.getValue();
+                Key = entry.getKey();
+            }
+
+            if (entry.getValue() < i) {
+
+                System.out.println(entry.getValue());
+                System.out.println(entry.getKey());
+                i = entry.getValue();
+                Key = entry.getKey();
+            }
+        }
+
+        return Key;
     }
 }
